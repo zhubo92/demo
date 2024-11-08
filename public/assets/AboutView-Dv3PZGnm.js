938b8b755c6f23443da1283a11f1195b858d61e9
朱波 <zhubo0327@gmail.com>
2024-11-08 17:42:04 +0800

    build

---
+++ public/assets/AboutView-Dv3PZGnm.js
@@ -0,0 +1,1 @@
+import{c as s,a,b as r,o as u,u as n}from"./index-DEP66wS4.js";const c={class:"about"},l={__name:"AboutView",setup(_){const e=n();function o(){e.go(-1)}return(i,t)=>(u(),s("div",c,[t[0]||(t[0]=a(" About ")),r("button",{onClick:o},"跳回")]))}};export{l as default};
